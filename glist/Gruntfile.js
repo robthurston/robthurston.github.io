3a1616d0c5263d2ab2b10e7af328c26477cfda30
rob-thurston <rob.thurston@gl05585m.gamesys.corp>
2016-09-13 11:58:33 +0100

    auto push via grunt

--- glist/Gruntfile.js
+++ glist/Gruntfile.js
@@ -1,17 +1,34 @@
 module.exports = function(grunt) {
-    //
-    // grunt.initConfig({
-    //     gitadd: {
-    //         task: {
-    //             options: {
-    //                 force: true
-    //             },
-    //             files: {
-    //                 src: ['test.txt']
-    //             }
-    //         }
-    //     },
-    // });
+
+    grunt.initConfig({
+        gitadd: {
+            task: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: ['*']
+                }
+            }
+        },
+        gitcommit: {
+            your_target: {
+                options: {
+                    message:"auto push via grunt"
+                },
+                files: {
+                    src: ['*']
+                }
+            }
+        },
+        gitpush: {
+            your_target: {
+                options: {
+                    // Target-specific options go here.
+                }
+            }
+        }
+    });
 
     grunt.loadNpmTasks('grunt-git');
 };
