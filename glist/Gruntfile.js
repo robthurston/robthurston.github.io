b1e94f3f285ab6eb37eba6383072ea1a7451c72c
rob-thurston <rob.thurston@gl05585m.gamesys.corp>
2016-09-13 11:37:47 +0100

    added npm and grunt

---
+++ glist/Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    //
+    // grunt.initConfig({
+    //     gitadd: {
+    //         task: {
+    //             options: {
+    //                 force: true
+    //             },
+    //             files: {
+    //                 src: ['test.txt']
+    //             }
+    //         }
+    //     },
+    // });
+
+    grunt.loadNpmTasks('grunt-git');
+};
